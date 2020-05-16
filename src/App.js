8f0b0a660a6449fa0d1c3c6f0f12a56dc0262075
Chris Stangle <chris.stangle@gmail.com>
2020-05-16 17:12:51 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { Button } from '@material-ui/core';
+import { ListItem } from '@material-ui/core';
 
 function App() {
+  const [pokemon, setPokemon] = useState(null)
+  
+  const handlePokemon = () => {
+    fetch("https://pokeapi.co/api/v2/pokemon/?limit=807")
+      .then(res => res.json())
+      .then(res => setPokemon(res.results))
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button 
+        variant = "contained"
+        onClick = {handlePokemon}
+        >Fetch Pokemon</Button>
+      {
+        pokemon ? <div>
+                    <ul className="list-group">
+                        {pokemon.length > 0 && pokemon.map((pokemon, i) => {
+                          return (<ListItem alignItems="center" key={i}>{pokemon.name}</ListItem>)
+                      })}
+                    </ul>
+                  </div>
+                : <p>Click Button to fetch pokemon!</p>
+      }
     </div>
   );
 }
