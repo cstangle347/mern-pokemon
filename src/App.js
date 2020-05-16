20bc4247cdd1580de4a3a9e027d9128832adb711
Chris Stangle <chris.stangle@gmail.com>
2020-05-16 18:46:52 -0500

    used axios, experimented w/ Material UI

--- src/App.js
+++ src/App.js
@@ -1,33 +1,41 @@
 import React, { useState } from 'react';
 import './App.css';
-import { Button } from '@material-ui/core';
+import { Button, Grid } from '@material-ui/core';
 import { ListItem } from '@material-ui/core';
+import axios from 'axios';
 
 function App() {
   const [pokemon, setPokemon] = useState(null)
   
   const handlePokemon = () => {
-    fetch("https://pokeapi.co/api/v2/pokemon/?limit=807")
-      .then(res => res.json())
-      .then(res => setPokemon(res.results))
+    axios.get("https://pokeapi.co/api/v2/pokemon/?limit=807")
+      .then(res => setPokemon(res.data.results))
+      .catch(err => console.log(err));
   }
-  
+
   return (
     <div className="App">
-      <Button 
-        variant = "contained"
-        onClick = {handlePokemon}
-        >Fetch Pokemon</Button>
-      {
-        pokemon ? <div>
-                    <ul className="list-group">
-                        {pokemon.length > 0 && pokemon.map((pokemon, i) => {
-                          return (<ListItem alignItems="center" key={i}>{pokemon.name}</ListItem>)
-                      })}
-                    </ul>
-                  </div>
-                : <p>Click Button to fetch pokemon!</p>
-      }
+      <Grid
+        //container
+        direction="row"
+        justify="center"
+        alignItems="center"
+        >
+        <Button 
+          variant = "contained"
+          onClick = {handlePokemon}
+          >Fetch Pokemon</Button>
+        {
+          pokemon ? <div>
+                      <ul className="list-group">
+                          {pokemon.length > 0 && pokemon.map((pokemon, i) => {
+                            return (<ListItem alignItems="center" key={i}>{pokemon.name}</ListItem>)
+                        })}
+                      </ul>
+                    </div>
+                  : <p>Click Button to fetch pokemon!</p>
+        }
+      </Grid>
     </div>
   );
 }
